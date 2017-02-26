bd6cde83429de13f233da7329228e83b93fe462c
adolfopalma <adolfo.palmagr@gmail.com>
2017-02-26 12:39:14 +0100

    Add files via upload

---
+++ ModAdolfo/__openerp__.py
@@ -0,0 +1,9 @@
+﻿{
+    'name': 'AdolfoAlbunes',
+    'description': 'Modulo de musica.',
+    'author': 'AdolfoPalma',
+    'depends': ['mail'],
+    'application': True,
+    'data' : ['aplicacionEjemplo01View.xml'],
+
+}
